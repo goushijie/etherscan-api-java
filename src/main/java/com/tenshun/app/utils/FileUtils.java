fc93bebb9b2a961048b569dcb84856a02a5b8bd2
robert.sadretdinov <robert.sadretdinov@rtlabs.ru>
2017-10-18 00:36:06 +0300

    ether address validation

--- src/main/java/com/tenshun/app/utils/FileUtils.java
+++ src/main/java/com/tenshun/app/utils/FileUtils.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class FileUtils {
@@ -23,7 +24,13 @@ public class FileUtils {
     public static List<String> extractAddressesFromFile(String filePath) throws IOException {
         return Files.lines(Paths.get(filePath))
                 .flatMap(post -> Arrays.stream(post.split("\\s+")))
-                .filter(splitted -> splitted.contains("0x") && splitted.length() == 42)
+                .filter(spl -> isValidAddress(spl, true))
                 .collect(Collectors.toList());
+    }
+
+    public static boolean isValidAddress(String address, boolean withPrefix) {
+        if(withPrefix || address.length() == 42) {
+            return Pattern.matches("^0x[a-fA-F0-9]{40}$", address);
+        } else return Pattern.matches("^[a-fA-F0-9]{40}$", address);
     }
 }
